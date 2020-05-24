f85e01c030d86faeca247f04d90ce7541e91ae5e
Adil Gondal <adil.gondal@yale.edu>
2020-05-24 02:07:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,10 @@
 # require modules here
 require "yaml"
-hash = YAML.load_file('lib/emoticons.yml')
-pp hash
+
 
 def load_library
-  # code goes here
+  hash = YAML.load_file('lib/emoticons.yml')
+  pp hash
 end
 
 def get_japanese_emoticon
